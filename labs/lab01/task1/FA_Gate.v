f388829fc2481ae39443ee3f41bd306f5b1be4c3
neelansh-gupta-20251362 <f20251362@pilani.bits-pilani.ac.in>
2026-08-29 07:47:44 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -16,9 +16,9 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
+  and #(2) (pc1, a,   b);
   xor #(2) (ps,  a,   b);
   xor #(2) (sum, cin, ps);
-  and #(2) (pc1, a,   b);
   and #(2) (pc2, cin, ps);
   or  #(2) (cout, pc1, pc2);
 
